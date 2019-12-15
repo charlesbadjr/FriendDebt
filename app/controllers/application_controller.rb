d46936baf6f6620d2f56397ae582f7e24dc41c63
Charlie <charlesbadjr@gmail.com>
2019-12-15 15:45:40 -0700

    fixed some routing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
     before_action :authenticate_user!
     before_action :configure_permitted_parameters, if: :devise_controller?
-      
+    skip_before_action :authenticate_user!, only: [:about, :home, :blog]
+
     private
   
     def configure_permitted_parameters
---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,14 @@
+class StaticPagesController < ApplicationController
+ 
+ 
+  def about
+  end
+
+  def home
+  end
+
+  def blog
+  end
+
+  
+end
